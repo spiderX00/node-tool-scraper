4d32aea6adc3a0c952ebd1e2ff1fe237525cb433
rdifelice <re.difelice@gmail.com>
2017-01-27 15:17:06 +0100

    ******

---
+++ properties.js
@@ -0,0 +1,11 @@
+const BUILD_PATH = __dirname + "/build/";
+const REGEX_ATTRIBUTES = [
+    "*[ng-model]",
+    "*[ng-scope]",
+    "*[ng-binding]",
+    "*[ng-repeat]",
+    "*[ng-disabled]"
+];
+
+module.exports.BUILD_PATH = BUILD_PATH;
+module.exports.REGEX_ATTRIBUTES = REGEX_ATTRIBUTES;
